1d249b8b035695aa23c9f345eab944132842df1d
astr000naut <10tincnh@gmail.com>
2023-05-29 22:52:51 +0700

    separate css file

--- src/components/base/BaseRadiogroup.vue
+++ src/components/base/BaseRadiogroup.vue
@@ -28,27 +28,8 @@ defineEmits(["update:radioValue"]);
 //#endregion
 </script>
 
-<style scoped>
-.radio__label {
-  font-weight: 500;
-}
-
-.radio__container {
-  display: flex;
-  column-gap: 12px;
-  margin-top: 12px;
-}
-
-.radio__item > input[type="radio"] {
-  width: 18px;
-  height: 18px;
-  margin-right: 8px;
-  accent-color: rgb(19, 155, 19);
-  transform: translate(4px, 4px);
-  cursor: pointer;
-}
-
-.radio__item > input[type="radio"]:focus {
-  outline-color: var(--clr-lg500);
-}
-</style>
+<style
+  scoped
+  lang="css"
+  src="../../css/components/base/base-radiogroup.css"
+></style>
--- src/components/views/under-development/UnderDevelopment.vue
+++ src/components/views/under-development/UnderDevelopment.vue
@@ -6,25 +6,8 @@
   </div>
 </template>
 
-<style scoped>
-.container {
-  width: 100%;
-  height: calc(100vh - 56px);
-  background-color: #8a8a8a8a;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.content {
-  padding: 150px;
-  background-color: #fff;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 15px;
-}
-.text {
-  font-size: 24px;
-}
-</style>
+<style
+  scoped
+  lang="css"
+  src="../../../css/components/views/under-development/under-development.css"
+></style>
