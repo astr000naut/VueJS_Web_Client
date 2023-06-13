81bdf57e031d36c068bc5d0f662ff2a6cded0f8a
astr000naut <10tincnh@gmail.com>
2023-06-13 23:11:43 +0700

    fix delete message

--- src/js/resources/message.js
+++ src/js/resources/message.js
@@ -2,7 +2,7 @@ const $message = {
   employeeDeleteConfirm: (code) => `Bạn có muốn xóa nhân viên <${code}> ?`,
   employeeDeleted: "Nhân viên đã bị xóa khỏi hệ thống",
   employeeMultipleDeleteConfirm: (amount) =>
-    `Bạn có chắc chắn muốn xóa ${amount} Nhân viên ?`,
+    `Bạn có chắc chắn muốn xóa ${amount} nhân viên ?`,
   employeeMultipeDeleted: (amount) => `Xóa thành công ${amount} nhân viên`,
   employeeCreated: "Thêm mới nhân viên thành công",
   employeeUpdated: "Sửa thông tin nhân viên thành công",
