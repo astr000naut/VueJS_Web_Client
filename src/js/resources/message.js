3d678f8fb72cc8ad4b9be2eb5eeca9cb62051293
astr000naut <10tincnh@gmail.com>
2023-06-15 20:06:53 +0700

    fix typo

--- src/js/resources/message.js
+++ src/js/resources/message.js
@@ -1,8 +1,8 @@
 const $message = {
-  employeeDeleteConfirm: (code) => `Bạn có muốn xóa nhân viên <${code}> ?`,
+  employeeDeleteConfirm: (code) => `Bạn có muốn xóa nhân viên <${code}>?`,
   employeeDeleted: "Nhân viên đã bị xóa khỏi hệ thống",
   employeeMultipleDeleteConfirm: (amount) =>
-    `Bạn có chắc chắn muốn xóa ${amount} nhân viên ?`,
+    `Bạn có chắc chắn muốn xóa ${amount} nhân viên?`,
   employeeMultipeDeleted: (amount) => `Xóa thành công ${amount} nhân viên`,
   employeeCreated: "Thêm mới nhân viên thành công",
   employeeUpdated: "Sửa thông tin nhân viên thành công",
